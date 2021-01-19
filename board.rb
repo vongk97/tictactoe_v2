0c025625ff3c88967656502768731ca9fa750967
Kevin Vong <vongk97@gmail.com>
2021-01-18 18:18:43 -0800

    Prompt player input

--- board.rb
+++ board.rb
@@ -7,4 +7,6 @@ class Board
     def initialize
         @@cells = [1,2,3,4,5,6,7,8,9]
     end
+
+    
 end
--- game.rb
+++ game.rb
@@ -1,19 +1,27 @@
 require_relative "board.rb"
 
 class Game
+    @p1
+    @p2
     def initialize
         board = Board.new
     end
 
     def play
         set_player_names
+        place_marker(p1)
+        place_marker(p2)
+    end
+
+    def place_marker(player)
+        puts "#{player} turn. Enter a value from 1-9 to place your marker."
     end
 
     def set_player_names
         puts "TIC TAC TOE!!! \n\n"
         print "Enter P1 Name: "
-        p1 = gets.chomp
+        @p1 = gets.chomp
         print "Enter P2 Name: "
-        p2 = gets.chomp
+        @p2 = gets.chomp
     end
 end
